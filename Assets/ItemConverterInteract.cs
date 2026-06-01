7d192f7c67694a159ccfcb40c2107a3597d8945d
waimai <gaohongyu47@gamil.com>
2026-06-01 20:41:11 +0800

    6.1 28

--- Assets/ItemConverterInteract.cs
+++ Assets/ItemConverterInteract.cs
@@ -1,74 +1,202 @@
 using System.Collections;
+
 using System.Collections.Generic;
+
 using System;
+
 using Microsoft.VisualBasic;
+
 using UnityEngine;
 
-public class ItemConverterInteract : Interactable
+
+
+public class ItemConvertorData
+
 {
+
+    public ItemSlot itemSlot;
+
+    public float timer;
+
+
+
+    public ItemConvertorData()
+
+    {
+
+        itemSlot = new ItemSlot();
+
+    }
+
+}
+
+
+
+public class ItemConverterInteract : Interactable, IPersistant
+
+{
+
     [SerializeField] Item convertableItem;
+
     [SerializeField] Item productedItem;
+
     [SerializeField] int productedItemCount = 1;
 
 
-    ItemSlot itemSlot;
 
     [SerializeField] float timeToProcess = 5f;
-    float timer;
+
+   
+
+    ItemConvertorData data;
+
+
+
+    Animator animator;
+
+
 
     private void Start()
+
     {
-        itemSlot = new ItemSlot();
+
+        if(data == null)
+
+        {
+
+            data = new ItemConvertorData();
+
+        }
+
+       
+
+        animator = GetComponent<Animator>();
+
     }
+
+
 
     public override void Interact(Character character)
+
     {
-        
-        if(itemSlot.item == null)
+
+       
+
+        if(data.itemSlot.item == null)
+
         {
+
             if (GameManager.instance.dragAndDropController.Check(convertableItem))
+
             {
-                
+
+               
+
                 StarItemProcessing();
+
             }
-        }
-        if(itemSlot.item != null && timer <= 0f)
-        {
-            GameManager.instance.inventoryContainer.Add(itemSlot.item, itemSlot.count);
-            itemSlot.Clear();
+
         }
 
-        
+        if(data.itemSlot.item != null && data.timer <= 0f)
+
+        {
+
+            GameManager.instance.inventoryContainer.Add(data.itemSlot.item, data.itemSlot.count);
+
+            data.itemSlot.Clear();
+
+        }
+
+
+
+       
+
+
 
     }
+
+
 
     private void StarItemProcessing()
+
     {
-        itemSlot.Copy(GameManager.instance.dragAndDropController.itemSlot);
+
+        animator.SetBool("Working", true);
+
+        data.itemSlot.Copy(GameManager.instance.dragAndDropController.itemSlot);
+        data.itemSlot.count = 1;
+
         GameManager.instance.dragAndDropController.RemoveItem();    
 
-        timer = timeToProcess;
+
+
+        data.timer = timeToProcess;
+
     }
+
+
 
     private void Update()
+
     {
-        if(itemSlot == null){return;}
-        if(timer > 0f)
+
+        if(data.itemSlot == null){return;}
+
+        if(data.timer > 0f)
+
         {
-            timer -= Time.deltaTime;
-            if(timer <= 0f)
+
+            data.timer -= Time.deltaTime;
+
+            if(data.timer <= 0f)
+
             {
+
                 CompleteItemConversion();
-                
+
+               
+
             }
+
         }
+
     }
 
-     private void CompleteItemConversion()
-    {
-        itemSlot.Clear();  
-        itemSlot.Set(productedItem, productedItemCount);
-    } 
 
-  
+
+     private void CompleteItemConversion()
+
+    {
+
+        animator.SetBool("Working", false);
+
+        data.itemSlot.Clear();  
+
+        data.itemSlot.Set(productedItem, productedItemCount);
+
+    }
+
+
+
+ 
+
+    public string Read()
+
+    {
+
+        return JsonUtility.ToJson(data);
+
+    }
+
+
+
+    public void Load(string jsonString)
+
+    {
+
+        data = JsonUtility.FromJson<ItemConvertorData>(jsonString);
+
+    }
+
 }
